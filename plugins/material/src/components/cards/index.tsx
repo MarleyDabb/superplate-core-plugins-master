26fe8b6ba89d50230c29de042c70c891a0470ad8
User2022 <marleydabb55@gmail.com>
2021-05-20 13:49:53 +0100

    cards update

--- plugins/material/src/components/cards/index.tsx
+++ plugins/material/src/components/cards/index.tsx
@@ -6,19 +6,46 @@ import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
+import data from "@public/meta.json";
+
+const useStyles = makeStyles({
+    root: {
+        minWidth: 275,
+    },
+    bullet: {
+        display: 'inline-block',
+        margin: '0 2px',
+        transform: 'scale(0.8)',
+    },
+    title: {
+        fontSize: 14,
+    },
+    pos: {
+        marginBottom: 12,
+    },
+});
+
 export const Cards: React.FC = (context) => {
     console.log(context);
+    const classes = useStyles();
     return (
-        <Card>
-            <CardContent>
-                <Typography>
-                    Test
-                </Typography>
-            </CardContent>
-            <CardActions>
-                <Button size={'small'}>Learn More</Button>
-            </CardActions>
-        </Card>
+        <>
+            {(data?.plugins ?? []).map((plugin) => (
+                <Card>
+                    <CardContent>
+                        <Typography className={classes.title}>
+                            {plugin.name}
+                        </Typography>
+                        <Typography variant={'body2'} component={'p'}>
+                            {plugin.description}
+                        </Typography>
+                    </CardContent>
+                    <CardActions>
+                        <Button size={'small'}>Learn More</Button>
+                    </CardActions>
+                </Card>
+            ))}
+        </>
     )
 }
 
