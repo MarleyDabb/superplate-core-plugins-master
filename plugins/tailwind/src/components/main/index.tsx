d0ef51c5bbdf57de851a152669ec8cf5bbc1c827
User2022 <marleydabb55@gmail.com>
2021-05-26 09:37:07 +0100

    Update index.tsx

--- plugins/tailwind/src/components/main/index.tsx
+++ plugins/tailwind/src/components/main/index.tsx
@@ -13,25 +13,6 @@ export const Main: React.FC = () => {
 
   return (
     <div className="text-center font-light py-5 bg-gray-700">
-        <% if(answers.authentication === "nextauth" ) { %>
-        {!session && (
-            <>
-                <p>Not Signed In</p>
-                <button
-                    className={`py-2 px-4 rounded bg-green-500 hover:bg-green-600 focus:outline-none ring-opacity-75 ring-green-400 focus:ring text-white text-lg`}
-                    onClick={signIn}>Sign In</button>
-            </>
-        )}
-        {session && (
-            <>
-                <p>Signed in as {session.user.email}</p>
-                <button
-                    className={`py-2 px-4 rounded bg-green-500 hover:bg-green-600 focus:outline-none ring-opacity-75 ring-green-400 focus:ring text-white text-lg`}
-                    onClick={signOut}>Sign Out</button>
-            </>
-        )}
-        <% } %>
-
       <div className="container mx-auto">
         <h1 <% if(!(e2etest === "none")) { %>  data-test="main-heading" <% } %> className="text-white text-8xl mb-2">superplate</h1>
         <p className="text-lg text-white mb-3">
